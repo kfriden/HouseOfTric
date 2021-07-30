ab0ce3ecd1f9cb77749e56ec17d36fb1f11bea6a
Kaitlyn Friden <kfriden@verizon.net>
2021-07-30 13:09:29 -0400

    small edits to login and auth component

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,44 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component }  from 'react';
 
 class Auth extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state= {
-            email: "",
-            password: ""
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        console.log("Handle event", this.state.email, this.state.password);
-    }
-
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-        console.log("Handle change", event);
-    }
     render() {
         return (
-            <div className="auth-center">
-                <h2>Login</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
-                    <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+            <div>
+                
+                <form className="auth-center">
+                    <h2>Login</h2>
+                    <input type="email" name="email" placeholder="Email" value={email} onChange={this.onChange} error={errors.email}/>
+                    <input type="password" name="password" placeholder="Password" />
 
                     <div>
-                        <button type="submit">Login</button>
+                        <button className="submit__btn" type="submit">Login</button>
                     </div>
                 </form>
             </div>
         );
-    }
+    };
 }
+       
+        
+
 
 export default Auth;
